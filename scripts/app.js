caf525bc3d78bbbce18d255333d46a1706a39b61
natalia <naliamate@gmail.com>
2018-09-24 16:13:32 -0500

    Pruebas con variable jovascript

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+/*
+
+var numero1 = 10.5;
+var numero2 = 5;
+var resultado = numero1*numero2;
+
+console.log(resultado);
+
+var nombre = "Natalia Mateus";
+var profesion = 'Desarrollador full stack';
+
+console.log(nombre + profesion);
+
+var tienen_mascota = true;
+
+console.log(tienen_mascota);
+
+var datos=("Natalia",36,false,25,78,"Desarrollador");
+
+console.log(datos[2]);
+*/
+
+/*
+var edad = 41;
+
+if(edad>=18 && edad<=40){
+	console.log("Eres un adulto joven");
+}else {
+	console.log("No eres un adulto joven");
+}
+
+var dias= ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+
+for(var i=0;i<7;i++){
+	console.log(dias[i]);
+}
+*/
+
+function suma(numero1,numero2){
+	var resultado=numero1+numero2;
+	return resultado;
+}
+
+var res=suma(88,55);
+
+console.log(res);
